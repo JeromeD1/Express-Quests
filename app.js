0bf16f329ceceda0f4aeb72f1cc7ab185df80db9
Jerome Desouches <jerome.desouches@gmail.com>
2023-07-31 11:02:08 +0200

    get & post movies & users

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000; //?? 5000 fourni une valeur par défaut (=5000) si APP_PORT n'est pas fourni (déclaré dans .env)
 
 const welcome = (req, res) => {
@@ -18,6 +20,9 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUserById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", movieHandlers.postUser);
+
 
 app.listen(port, (err) => {
   if (err) {
