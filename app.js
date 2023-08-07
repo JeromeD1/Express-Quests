9921fad938c29a9f4f17de36bdc53faa076d657c
Jerome Desouches <jerome.desouches@gmail.com>
2023-08-07 15:52:09 +0200

    ajout d'un validator à l'aide du package joi

--- app.js
+++ app.js
@@ -16,16 +16,17 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
+const validator = require("./validator");
 
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/movies/:id", movieHandlers.putMovieById);
-app.put("/api/users/:id", userHandlers.putUserById);
+app.post("/api/movies", validator.validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validator.validateUser, userHandlers.postUser);
+app.put("/api/movies/:id", validator.validateMovie, movieHandlers.putMovieById);
+app.put("/api/users/:id", validator.validateUser,userHandlers.putUserById);
 app.delete("/api/movies/:id", movieHandlers.deleteMovieById);
 app.delete("/api/users/:id", userHandlers.deleteUserById);
 
